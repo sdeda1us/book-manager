44f7ff2be99608a96f662bd60f298a701706302c
sdeda1us <steven.amloney@gmail.com>
2020-12-23 11:42:59 -0600

    form render basic setup working

--- src/components/AddBook/AddBook.js
+++ src/components/AddBook/AddBook.js
@@ -2,10 +2,22 @@ import React from 'react';
 import Form from '../Form/Form';
 
 export default function AddBook () {
+    
+    const formData = 
+        [
+            {size: "100%", type: "text", label: "Title"}, 
+            {size: "25%", type: "checkbox", label: "Genre" },
+            {size: "25%", type: "text", label: "Year Published"},
+            {size: "25%", type: "text", label: "Pages"}
+        ];
+
+        //add style objects for fields
+        
     return (
         <div className="book-form">
             <h2>Add Book</h2>
-            <Form />
+            <Form formData={formData}/>
         </div>
     )
 }
+
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 
 
-export default function Form () {
+export default function Form ({formData}) {
     return (
         <div className="form">
             <p>Hi From form!</p>
+            {JSON.stringify(formData.labels)}
+            <form>
+                {formData.map(input => 
+                    <div><label>{input.label}</label><input type={input.type}/></div>
+                    )}
+            </form>
         </div>
     )
 }
