921ce20a68f4cca90d70e17e1b7faca2a7b861b2
sdeda1us <steven.amloney@gmail.com>
2020-12-29 23:03:42 -0600

    more styling

--- src/components/AddBook/AddBook.js
+++ src/components/AddBook/AddBook.js
@@ -2,7 +2,47 @@ import React, {useEffect, useState} from 'react';
 import BookList from '../BookList/BookList';
 import { useDispatch, useSelector} from 'react-redux';
 import {useInput} from '../Hooks/Hooks';
+import styled from 'styled-components';
 
+const PageSpace = styled.div`
+  padding: 10px;
+  width: auto;
+`
+
+const InputField = styled.input`
+    border-radius: 5px;
+    box-shadow: 10px 10px 5px gray;
+    margin: 10px;
+`
+const SelectField = styled.select`
+    border-radius: 5px;
+    box-shadow: 8px 8px 5px gray;
+    margin: 10px;
+    width: 400px;
+    height: 20px;
+`
+const SubmitButton = styled.button`
+    border-radius: 5px;
+    box-shadow: 8px 8px 5px gray;
+    margin: 10px;
+    background-color: green;
+    color: white;
+    height: 40px;
+    width: 65px;
+`
+const LabelBox = styled.div`
+    width: 20%;
+`
+
+const InputBox = styled.div`
+width: 70%;
+`
+
+const FormRow = styled.div`
+    display: inline-flex;
+    width: 95%;
+    justify-content: space-around;
+`
 export default function AddBook () {
     //imports redux state for book subjects
     const subjectList = useSelector(state => state.subjectReducer);
@@ -31,31 +71,44 @@ export default function AddBook () {
     //render properties for input fields
     const formData = 
         [
-            {style: {id: 1, width:"60%", border: "2px solid red"}, type: "text", label: "Title", varName: "title", function: titleProps}, 
-            {style: {id: 2, width:"25%"}, type: "text", label: "Year Published", varName: "year_published", function: publishedProps},
-            {style: {id: 3, width:"25%"}, type: "text", label: "Pages", varName: "pages", function: pagesProps}
+            {style: {id: 1, width:"80%"}, type: "text", label: "Title", varName: "title", function: titleProps}, 
+            {style: {id: 2, width:"20%"}, type: "text", label: "Year Published", varName: "year_published", function: publishedProps},
+            {style: {id: 3, width:"20%"}, type: "text", label: "Pages", varName: "pages", function: pagesProps}
         ];
 
    
     return (
-        <div className="book-form">
+        <PageSpace>
             <h2>Add Book</h2>
             <form onSubmit={submit}>
                 {formData.map((input) => 
-                    <div key={input.id}><label>{input.label}</label>
-                        <input {...input.function} type={input.type} style={input.style}/>
-                    </div>
-                    )}
-                <label>Subject</label>
-                <select name="subject" {...subjectProps}>
-                    <option>Choose a subject</option>
-                    {subjectList.map((s) => (<option key={s.id} value={s.id}>{s.subject}</option>))}
-                </select>
-                <button>Submit</button>
+                    <FormRow>
+                        <LabelBox key={input.id}>
+                            <label>{input.label}</label>
+                        </LabelBox>
+                        <InputBox>  
+                            <InputField {...input.function} 
+                                type={input.type} style={input.style}/>
+                        </InputBox>
+                    </FormRow>)}
+                <FormRow>
+                    <LabelBox>
+                        <label>Subject</label>
+                    </LabelBox>
+                    <InputBox>
+                        <SelectField name="subject" {...subjectProps}>
+                            <option>Choose a subject</option>
+                            {subjectList.map((s) => (<option key={s.id} value={s.id}>{s.subject}</option>))}
+                        </SelectField>
+                    </InputBox>
+                </FormRow>
+                <FormRow>
+                    <SubmitButton>Submit</SubmitButton>
+                </FormRow>
             </form>
             <h2>Book Collection</h2>
             <BookList/>
-        </div>
+        </PageSpace>
     )
 }
 
